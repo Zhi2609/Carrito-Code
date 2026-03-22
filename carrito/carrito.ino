433db7a5b28864194e7dec7599637be80f39b0f8
Cesar Garcia <cesaremiliogarciagtz254@gmail.com>
2026-03-22 11:34:11 -0600

    Create carrito.ino

---
+++ carrito/carrito.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  // Si se ocupab librerias especiales pon el nombre aqui usando un comentario
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
