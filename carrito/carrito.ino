73339e88d2860eafc2e87be830aa60732d124843
Cesar Garcia <cesaremiliogarciagtz254@gmail.com>
2026-04-16 09:40:43 -0600

    Primer version de Prueba
    Tendria que funcionar, el codigo implementa los modulos que tienen que ver con los sensores como modo 1, 2 y 4

--- carrito/carrito.ino
+++ carrito/carrito.ino
@@ -1,10 +1,77 @@
-void setup() {
-  // put your setup code here, to run once:
-  // Si se ocupab librerias especiales pon el nombre aqui usando un comentario
+#include <TinyGPSPlus.h>
 
+const int TRIG_PIN = 5;
+const int ECHO_PIN = 18;
+
+const int pinesIR[8] = {13, 12, 14, 27, 26, 25, 33, 32}; // Cambiar segun los pines del ESP32
+int estadoLinea[8];
+
+TinyGPSPlus gps;
+HardwareSerial SerialGPS(2); 
+const int RX_GPS = 16;
+const int TX_GPS = 17;
+
+float distanciaObstaculo = 0.0;
+double latitudActual = 0.0;
+double longitudActual = 0.0;
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  for (int i = 0; i < 8; i++) {
+    pinMode(pinesIR[i], INPUT);
+  }
+
+  SerialGPS.begin(9600, SERIAL_8N1, RX_GPS, TX_GPS);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+float leerDistancia() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  
+  long duracion = pulseIn(ECHO_PIN, HIGH);
+  
+  float distancia = duracion * 0.034 / 2;
+  
+  distanciaObstaculo = distancia;
+  return distancia;
+}
 
+void leerSensoresLinea() {
+  Serial.print("Lectura de línea: ");
+  for (int i = 0; i < 8; i++) {
+    estadoLinea[i] = digitalRead(pinesIR[i]);
+
+    Serial.print(estadoLinea[i]); 
+    Serial.print(" ");
+  }
+  Serial.println();
+}
+
+void actualizarGPS() {
+  while (SerialGPS.available() > 0) {
+    if (gps.encode(SerialGPS.read())) {
+      if (gps.location.isValid()) {
+        latitudActual = gps.location.lat();
+        longitudActual = gps.location.lng();
+      }
+    }
+  }
+}
+
+void imprimirUbicacionGPS() {
+  if (gps.location.isValid()) {
+    Serial.print("Latitud: ");
+    Serial.print(latitudActual, 6);
+    Serial.print(" | Longitud: ");
+    Serial.println(longitudActual, 6);
+  } else {
+    Serial.println("GPS: Esperando señal de satélites...");
+  }
 }
